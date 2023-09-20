848aa6891db145b4ec483620769fa271519f175b
abb22axerox <axel.roxenborg@abbgymnasiet.se>
2023-09-20 13:52:25 +0200

    virus working

--- anteckningar/extremelygoodcode (1).py
+++ anteckningar/extremelygoodcode (1).py
@@ -1,8 +1,7 @@
 import subprocess
 
-for i in range(1, 3):
+for i in range(1, 4):
    f = open('Bot_'+ str(i) + '.py' , 'w')
-   f.write("import time \nimport random \nimport subprocess \nfor i in range(1,3): \n f = open('Hallo_' + str(i) + str(random.randint(1,999)), 'w') \n f.close() \n time.sleep(0.1) \nsubprocess.run(['python', 'Bot_2.py'])")
+   f.write("import time\nimport random\nfor i in range(1,30):\n  f = open('Hallo_' + str(i) + str(random.randint(1,999)), 'w')\n  f.close() \n  time.sleep(1)")
    f.close()
-
-subprocess.run(['python', 'Bot_1.py'])
+   subprocess.run(['python', 'Bot_' + str(i) + '.py'])
