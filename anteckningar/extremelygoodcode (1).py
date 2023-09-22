7a3729642c91831645ad6f9509427e4d41222c0e
abb22axerox <axel.roxenborg@abbgymnasiet.se>
2023-09-22 08:08:37 +0200

    changes

--- anteckningar/extremelygoodcode (1).py
+++ anteckningar/extremelygoodcode (1).py
@@ -1,12 +1,14 @@
 # import subprocess
 import os
 
+bot_code = "import time\nimport random\nfor i in range(1,10):\n  f = open('Hallo_' + str(i) + str(random.randint(1,999)), 'w')\n  f.write('Lol this is a virus!')\n  f.close()\n  time.sleep(0.01)"
+
 def execute_python_file(file_path):
    os.system(f'python {file_path}')
 
 for i in range(1, 11):
    f = open('Bot_'+ str(i) + '.py' , 'w')
-   f.write("import time\nimport random\nfor i in range(1,10):\n  f = open('Hallo_' + str(i) + str(random.randint(1,999)), 'w')\n  f.write('Lol this is a virus!')\n  f.close()\n  time.sleep(0.01)")
+   f.write(bot_code)
    f.close()
    # subprocess.run(['python', 'Bot_' + str(i) + '.py'])
    execute_python_file('Bot_'+ str(i) + '.py')
