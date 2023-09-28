ef22aebd8cd2fa46d5dc39fb2acaf442af9ed481
abb22axerox <axel.roxenborg@abbgymnasiet.se>
2023-09-28 10:55:22 +0200

    veckouppgift

---
+++ veckouppgifter/vecka 5/veckouppgift_5.py
@@ -0,0 +1,85 @@
+import os
+import json
+
+database_path = 'python_kurs-1/veckouppgifter/vecka 5/notes.json'
+
+f = open(database_path)
+notes = json.loads(f.read())
+f.close()
+
+no_match = True
+
+def clear_terminal():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    
+def print_list():
+    for note in notes:
+        print(f'- {note}')
+
+def print_basic_message():
+    clear_terminal()
+
+    print('''.:  ALWAYSNOTE  :.
+-- gold edition --
+******************''')
+    
+    print_list()
+    
+    print('''------------------
+view | view note
+add  | add note
+rm   | remove note
+exit | exit program
+------------------''')
+
+print_basic_message()
+
+while True:
+    print_basic_message()
+
+    selection = input('menu > ')
+    print('------------------')
+
+    if selection == 'view':
+        title = input('title > ')
+        for note in notes:
+            if title == note:
+                print(f'''------------------
+{notes[note]}
+------------------''')
+                no_match = False
+                
+        if no_match:
+            print('''------------------
+ERROR: Unknown note
+------------------''')
+
+    elif selection == 'add':
+        title = input('title > ')
+        descr = input('descr > ')
+        notes[title] = descr
+        print('''------------------
+INFO: Note added
+------------------''')
+            
+    elif selection == 'rm':
+        title = input('title > ')
+        del notes[title]
+        print('''------------------
+INFO: Note deleted
+------------------''')
+
+    elif selection == 'exit':
+        f = open(database_path, "w")
+        f.write(json.dumps(notes))
+        f.close()
+        print(f'''INFO: Saved successfully to "{database_path}"
+------------------''')
+        break
+
+    else:
+        print(f'''------------------
+ERROR: Unknown command ({selection})
+------------------''')
+
+    input('Press enter to continue...')
