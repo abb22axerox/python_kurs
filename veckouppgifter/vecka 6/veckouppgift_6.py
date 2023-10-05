5badb420f1333ca051ceb22c3a374769079f6bab
abb22axerox <axel.roxenborg@abbgymnasiet.se>
2023-10-05 15:32:24 +0200

    veckouppgift 6

--- veckouppgifter/vecka 6/veckouppgift_6.py
+++ veckouppgifter/vecka 6/veckouppgift_6.py
@@ -1,29 +1,35 @@
 import os
 import requests
 import json
-import time
 
-def clear_terminal():
-    os.system('cls' if os.name == 'nt' else 'clear')
+api_url = 'https://8fzqlwv0jd.execute-api.eu-north-1.amazonaws.com/'
 
+cities = requests.get(api_url)
+cities = json.loads(cities.text)
+
+# list cities function
 def list_cities(city):
     print(f'- {city.title()}')
 
-def print_basic_message():
-    clear_terminal()
+# clear terminal
+os.system('cls' if os.name == 'nt' else 'clear')
 
-    print('''.: JetSetPets :.
+print('''.: JetSetPets :.
 ----------------''')
 
-    cities = requests.get(api_url)
-    cities = json.loads(cities.text)
-    for city in cities['cities']:
-        list_cities(city)
+# list cities from api
+for city in cities['cities']:
+    list_cities(city)
+print(16 * '-')
 
-    print(16 * '-')
+city = (input('Select city > ')).lower()
 
-def pet_selection():
+if city not in cities['cities']:
     print('''----------------
+ERROR: City not found''')
+    exit()
+
+print('''----------------
 - bird
 - cat
 - dog
@@ -32,30 +38,29 @@ def pet_selection():
 - rabbit
 ----------------''')
 
-while True:
-    api_url = 'https://8fzqlwv0jd.execute-api.eu-north-1.amazonaws.com/'
+pet = (input('Select pet: ')).lower()
+print(16 * '-')
 
-    print_basic_message()
+valid_pets = ['bird', 'cat', 'dog', 'fish', 'mouse', 'rabbit']
 
-    city = (input('Select city > ')).lower()
+if pet not in valid_pets:
+    print('ERROR: Pet not found')
+    exit()
 
-    if city == 'stockholm':
-        pet = input('Select pet: ')
-        print(16 * '-')
+# get city url dictionary
+city_url = api_url + city
+r = requests.get(city_url)
+main_dict = json.loads(r.text)
 
-        api_url += '/' + city
-        r = requests.get(api_url)
-        main_dict = json.loads(r.text)
+for person in main_dict['users']:
+    # person url dictionary
+    person_url = city_url + '/' + person['id']
+    r = requests.get(person_url)
+    person_dict = json.loads(r.text)
 
-        for person in main_dict['users']:
-
-            person_api_url = api_url + '/' + person['id']
-            r = requests.get(person_api_url)
-            person_dict = json.loads(r.text)
-
-            for animal in person_dict['animals']:
-                if pet == animal['type']:
-                    print(f"{person_dict['forename']} {person_dict['surname']} has a {animal['type']} named {animal['name']}")
-        break
-
-
+    # loop animal list
+    for animal in person_dict['animals']:
+        # check if the type is correct
+        if pet == animal['type']:
+            # print results
+            print(f"{person_dict['forename']} {person_dict['surname']} has a {animal['type']} named {animal['name']}")
